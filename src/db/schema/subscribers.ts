9d644023ab362deac47b29e7fbb76cd37698d6bb
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-14 20:59:04 +0200

    add subscribers table schema

---
+++ src/db/schema/subscribers.ts
@@ -0,0 +1,12 @@
+import {pgTable, uuid, text , timestamp} from "drizzle-orm/pg-core"
+import { pipelines } from "./pipelines.js"
+
+export const subscribers = pgTable("subscribers" , {
+    id: uuid("id").defaultRandom().primaryKey(),
+    pipelineId: uuid("pipeline_id").references(()=>pipelines.id).notNull(),
+    url: text("url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type Subscriber = typeof subscribers.$inferSelect;
+export type NewSubscriber = typeof subscribers.$inferInsert;
