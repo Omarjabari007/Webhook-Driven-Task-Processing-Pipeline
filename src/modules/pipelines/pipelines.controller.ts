a2b3726b81f44e7e6aa489fae6ae5e0fd25f9a60
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-15 20:16:35 +0200

    Add createPipeline controller

---
+++ src/modules/pipelines/pipelines.controller.ts
@@ -0,0 +1,14 @@
+import type { Request , Response } from "express";
+import {createPipeline} from "./pipelines.service.ts"
+import type { CreatePipelineDTO } from "./pipelines.types.ts";
+
+export async function createPipelineController(req : Request , res : Response){
+    try{
+        const data = req.body as CreatePipelineDTO
+        const pipeline = await createPipeline(data)
+        res.status(201).json(pipeline)
+    }catch(error){
+        console.error(error)
+        res.status(500).json({message: "Failed to create pipeline"})
+    }
+}
