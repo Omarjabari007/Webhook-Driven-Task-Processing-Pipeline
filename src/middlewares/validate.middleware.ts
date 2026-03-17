56dd0935eee74ed69a918d0ec788fc9700c038b2
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-17 19:56:50 +0200

    Implement validation-middleware by zod

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,28 @@
+import type { z, ZodSchema, ZodTypeAny } from "zod";
+import type { Request , Response , NextFunction } from "express";
+import { AppError } from "../utils/AppError.ts";
+
+type RequestSchema = {
+  body?: unknown;
+  params?: Record<string, any>;
+  query?: Record<string, any>;
+};
+
+export function validate<T extends ZodTypeAny>(schema: T){
+    return(req: Request, res : Response ,next : NextFunction) => {
+        const result = schema.safeParse({
+            body: req.body,
+            params: req.params,
+            query: req.query
+        });
+        if(!result.success){
+            const message = result.error.issues.map((e) => e.message).join(", ");
+            return next(new AppError(message, 400));
+        }
+        const data = result.data as z.infer<T> & RequestSchema;
+        if(data.body) req.body = data.body;
+        if (data.params) req.params = data.params;
+        if (data.query) req.query = data.query;
+        next();
+    }
+}
--- src/modules/pipelines/pipelines.schema.ts
+++ src/modules/pipelines/pipelines.schema.ts
@@ -1,13 +1,15 @@
 import {z} from "zod"
 
 export const PipelineCreationSchema = z.object({
+    body: z.object({
     name: z.string().min(3),
     sourcePath: z.string().min(1),
     actionType: z.literal("aoe4_match_summary"),
     config: z.object({
         apiUrl: z.string().url()
     }).optional() 
-});
+})
+})
 
 export const pipelineIdSchema = z.object({
     id: z.string().uuid()
