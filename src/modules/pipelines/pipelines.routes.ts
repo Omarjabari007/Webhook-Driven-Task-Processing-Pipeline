bfbc326e579a8b87ed3b4c5fed9085a727baf024
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-17 20:31:09 +0200

    Implement the validation to routes

--- src/modules/pipelines/pipelines.routes.ts
+++ src/modules/pipelines/pipelines.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createPipelineController ,getPipelineByIdController , getPipelinesController,deletePipelineController } from "./pipelines.controller.ts";
 import { asyncHandler } from "../../utils/asyncHandler.ts";
+import { validate } from "../../middlewares/validate.middleware.ts";
+import { PipelineCreationSchema , pipelineIdSchema } from "./pipelines.schema.ts";
 
 const router = Router()
 
-router.post("/",createPipelineController)
-router.get("/", getPipelinesController)
-router.get("/:id" , asyncHandler(getPipelineByIdController))
-router.delete("/:id" , deletePipelineController)
+router.post("/",validate(PipelineCreationSchema),asyncHandler(createPipelineController))
+router.get("/", asyncHandler(getPipelinesController))
+router.get("/:id" , validate(pipelineIdSchema), asyncHandler(getPipelineByIdController))
+router.delete("/:id" , validate(pipelineIdSchema), asyncHandler(deletePipelineController))
 export default router
