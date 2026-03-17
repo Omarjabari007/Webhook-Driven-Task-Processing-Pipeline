652f33f2befc25706f58721de6b7c1206be1fb80
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-17 21:04:54 +0200

    Add subscirber zod schema

---
+++ src/modules/subscribers/subscribers.schema.ts
@@ -0,0 +1,16 @@
+import {z} from "zod"
+
+export const SubscriberCreationSchema = z.object({
+    params: z.object({
+        pipelineId: z.string().uuid()
+    }),
+    body: z.object({
+        url: z.string().url()
+    })
+});
+
+export const subscriberIdSchema = z.object({
+    params: z.object({
+        id: z.string().uuid()
+    })
+})
