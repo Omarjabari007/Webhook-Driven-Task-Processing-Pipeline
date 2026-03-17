8e56fcdd42daa795f5ad93b9f31097a17fd1f5e1
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-17 19:11:45 +0200

    Implement zod schema into the pipeline

---
+++ src/modules/pipelines/pipelines.schema.ts
@@ -0,0 +1,14 @@
+import {z} from "zod"
+
+export const PipelineCreationSchema = z.object({
+    name: z.string().min(3),
+    sourcePath: z.string().min(1),
+    actionType: z.literal("aoe4_match_summary"),
+    config: z.object({
+        apiUrl: z.string().url()
+    }).optional() 
+});
+
+export const pipelineIdSchema = z.object({
+    id: z.string().uuid()
+})
