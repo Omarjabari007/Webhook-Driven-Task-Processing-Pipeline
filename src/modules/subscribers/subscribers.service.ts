09574453c316152ef56bbd3cc37fa13c500c4a54
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-18 18:26:12 +0200

    Implement getSubsribe by pipelineID function

--- src/modules/subscribers/subscribers.service.ts
+++ src/modules/subscribers/subscribers.service.ts
@@ -5,6 +5,7 @@ import { eq } from "drizzle-orm";
 import { AppError } from "../../utils/AppError.ts";
 
 import type { CreateSubscriberDTO , SubscriberResponse } from "./subscribers.type.ts";
+import { pipe } from "zod";
 
 export async function createSubscriber(pipelineId: string , data: CreateSubscriberDTO)
 :Promise<SubscriberResponse>{
@@ -21,4 +22,8 @@ if (!subscriber) {
   throw new AppError("Failed to create subscriber", 500);
 }
 return subscriber;
+}
+
+export async function getSubscriberByPipeline(pipelineId: string): Promise<SubscriberResponse[]>{
+    return db.select().from(subscribers).where(eq(subscribers.pipelineId , pipelineId));
 }
