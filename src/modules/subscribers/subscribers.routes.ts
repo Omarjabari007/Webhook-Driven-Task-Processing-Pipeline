bc62e53c5d906d3573f96e3f9a058cd72ea35396
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-18 19:38:47 +0200

    Add delete subscriber route

--- src/modules/subscribers/subscribers.routes.ts
+++ src/modules/subscribers/subscribers.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createSubscriberController , getSubscribersController } from "./subscribers.controller.ts";
+import { createSubscriberController , getSubscribersController , deleteSubscriberController} from "./subscribers.controller.ts";
 
 import { validate } from "../../middlewares/validate.middleware.ts";
 import { asyncHandler } from "../../utils/asyncHandler.ts";
 
-import { SubscriberCreationSchema} from "./subscribers.schema.ts";
+import { SubscriberCreationSchema , subscriberIdSchema} from "./subscribers.schema.ts";
 
 const router = Router();
 
@@ -12,5 +12,8 @@ router.post("/pipelines/:pipelineId/subscribers",validate(SubscriberCreationSche
 asyncHandler(createSubscriberController));
 
 router.get("/pipelines/:pipelineId/subscribers", asyncHandler(getSubscribersController));
+
+router.delete("/subscribers/:id", validate(subscriberIdSchema),
+ asyncHandler(deleteSubscriberController));
 
 export default router;
