2e07b42750b34f2ee71112b5aa25ea2cc65150a5
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-21 19:29:54 +0200

    Enchance deliveries processor to accept discord webhook

--- src/modules/workers/deliveries.processor.ts
+++ src/modules/workers/deliveries.processor.ts
@@ -3,6 +3,7 @@ import { deliveries } from "../../db/schema/deliveries.ts";
 import { subscribers } from "../../db/schema/subscribers.ts";
 import { jobs } from "../../db/schema/jobs.ts";
 import fetch from "node-fetch";
+import type { MatchSummaryResult } from "./types.ts";
 
 import { eq } from "drizzle-orm";
 
@@ -34,31 +35,40 @@ export async function processPendingDeliveries() {
         throw new Error("Job result not ready");
       }
       console.log("Sending to:", sub.url);
-      const response = await fetch(sub.url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(job.result),
-      });
 
-      if (!response.ok) {
-        throw new Error(`HTTP ${response.status}`);
-      }
-      console.log("Delivery success:", delivery.id);
+      let body: string;
+      const result = job.result as MatchSummaryResult;
+      if (sub.url.includes("discord.com") && result?.summary) 
+        {
+        body = JSON.stringify(
+          {
+            content: result.summary
+          });
 
-      await db.update(deliveries).set({ status: "success" }).where(eq(deliveries.id, delivery.id));
+        } else {
+          body = JSON.stringify(job.result);
+        }
+        const response = await fetch(sub.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },body,});
 
-    } catch (err: any) {
-      console.error("Delivery failed:", delivery.id, err.message);
-      const newAttempts = delivery.attempts + 1;
-
-      await db.update(deliveries)
-        .set({
-          attempts: newAttempts,
-          lastError: err.message,
-          status: newAttempts >= MAX_RETRIES ? "failed" : "pending",
-        }).where(eq(deliveries.id, delivery.id));
+          if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+          }
+          console.log("Delivery success:", delivery.id);
+          await db.update(deliveries).set({ status: "success" }).where(eq(deliveries.id, delivery.id));
+          } 
+          catch (err: any) {
+            console.error("Delivery failed:", delivery.id, err.message);
+            const newAttempts = delivery.attempts + 1;
+            await db.update(deliveries)
+            .set({
+              attempts: newAttempts,
+              lastError: err.message,
+              status: newAttempts >= MAX_RETRIES ? "failed" : "pending",
+            }).where(eq(deliveries.id, delivery.id));
     }
   }
 }
