922deac315dfbe25c63f8e5d1d01415585c0f763
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-22 00:13:01 +0200

    Add second action aoe4profile into action registery

--- src/modules/actions/index.ts
+++ src/modules/actions/index.ts
@@ -1,5 +1,7 @@
 import { aoe4MatchSummaryAction } from "./aoe4.action.ts";
+import { aoe4ProfileAction } from "./aoe4_player_profile.ts";
 
 export const actions = {
   aoe4_match_summary: aoe4MatchSummaryAction,
+  aoe4_player_profile:aoe4ProfileAction,
 };
