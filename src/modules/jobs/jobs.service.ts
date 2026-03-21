772083ad73faf4dc34e30a2a08367e5fb4f7eb30
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-21 15:26:12 +0200

    Implement jobs-service to retrive jobs details and Job-Deliveries

---
+++ src/modules/jobs/jobs.service.ts
@@ -0,0 +1,26 @@
+import { db } from "../../db/index.ts";
+import { jobs } from "../../db/schema/jobs.ts";
+import { deliveries } from "../../db/schema/deliveries.ts";
+
+import { eq } from "drizzle-orm";
+import { AppError } from "../../utils/AppError.ts";
+
+export async function getJobs() {
+  return db.select().from(jobs);
+}
+
+export async function getJobById(id: string) {
+  const [job] = await db.select().from(jobs).where(eq(jobs.id, id));
+  if (!job) {
+    throw new AppError("Job not found", 404);
+  }
+  return job;
+}
+
+export async function getJobDeliveries(jobId: string) {
+  const job = await db.select().from(jobs).where(eq(jobs.id, jobId));
+  if (job.length === 0) {
+    throw new AppError("Job not found", 404);
+  }
+  return db.select().from(deliveries).where(eq(deliveries.jobId, jobId));
+}
