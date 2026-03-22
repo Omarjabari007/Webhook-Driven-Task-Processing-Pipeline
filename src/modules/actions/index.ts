d43cfdfbb08ec72003843b8c428b56faa9c10fec
Omarjabari007 <129548182+Omarjabari007@users.noreply.github.com>
2026-03-22 16:58:55 +0200

    feat: register aoe4MetaActin in action registery

--- src/modules/actions/index.ts
+++ src/modules/actions/index.ts
@@ -1,7 +1,9 @@
 import { aoe4MatchSummaryAction } from "./aoe4.action.ts";
 import { aoe4ProfileAction } from "./aoe4_player_profile.ts";
+import { aoe4MetaAction } from "./aoe4-meta.action.ts";
 
 export const actions = {
   aoe4_match_summary: aoe4MatchSummaryAction,
   aoe4_player_profile:aoe4ProfileAction,
+  aoe4MetaAction
 };
